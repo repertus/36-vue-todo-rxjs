212730edab6d0765e9b3315f2e58fbfaf05b78be
Robert MartinDelCampo <robertmdelc@gmail.com>
2018-04-22 12:38:24 -0700

    feature: Added vue-rx and RxJS libraries

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
+import VueRx from 'vue-rx'
+import { Observable } from 'rxjs/Observable'
 import Vuex from 'vuex'
 import App from './App.vue'
 import store from './store'
 
-Vue.use(Vuex)
+Vue.use(Vuex, VueRx, { Observable })
 
 const vuex = new Vuex.Store(store)
 
